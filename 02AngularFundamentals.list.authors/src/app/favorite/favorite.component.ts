d4edc3b9a16c6173962437ba374e0e022d72051f
DeltaRig <daniprigoli@gmail.com>
2023-02-18 13:25:46 -0300

    Aliasing Output properties

--- 02AngularFundamentals.list.authors/src/app/favorite/favorite.component.ts
+++ 02AngularFundamentals.list.authors/src/app/favorite/favorite.component.ts
@@ -5,17 +5,15 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css']
 })
-export class FavoriteComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-  @Output() change = new EventEmitter(); // EventEmmiter is not a primitive type in Typescript
-
+export class FavoriteComponent {
+  
   @Input('is-favorite') isFavorite: boolean | undefined; // the alias is optinal put recommended to reduce the impact if some day this variable change the name
+  @Output('change') change = new EventEmitter(); // EventEmmiter is not a primitive type in Typescript
+
 
   onClick() { 
     this.isFavorite = !this.isFavorite;
-    this.change.emit(newValue: this.isFavorite); // this notify others that something has happened
+    this.change.emit( { newValue: this.isFavorite } ); // this notify others that something has happened
   }
 }
 
