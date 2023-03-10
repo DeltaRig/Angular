ad12a2fc8cedf943d7db7f00e48b044577be6c64
Daniela_Rigoli <Daniela_Rigoli@dell.com>
2023-03-10 10:02:05 -0300

    templete

--- 02AngularFundamentals.list.authors/src/app/star/star.component.ts
+++ 02AngularFundamentals.list.authors/src/app/star/star.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'star',
-  template: `<br>TESTE<br>
-  <i (click)="onClick($event)" [class]="isFavorite ? 'bi-star' : 'bi-star-fill' " class="bi"></i>
-  `
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
 })
 
 export class StarComponent {
