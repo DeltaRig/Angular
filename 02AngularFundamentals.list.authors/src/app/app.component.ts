81e6f39c5f78df4c181843ac634e37ceedca3cf5
DeltaRig <hitomikaleiko@gmail.com>
2023-03-11 10:54:34 -0300

    ngFor change detection

--- 02AngularFundamentals.list.authors/src/app/app.component.ts
+++ 02AngularFundamentals.list.authors/src/app/app.component.ts
@@ -20,10 +20,18 @@ export class AppComponent {
   courses=[
     {title: "Angular's Course", students: 50, rating: 4.2, price:230.98},
     {title: "Cypress's Course", students: 30, rating: 3.2, price:130.98},
-    {title: "Network's Course", students: 60, rating: 4.4, price:180.00},
-    {title: "Network's Course", students: 60, rating: 4.4, price:180.00},
     {title: "Network's Course", students: 60, rating: 4.4, price:180.00}
   ];
 
+  onAddCourse(){
+    this.courses.push({title: "React's Course", students: 0, rating: 0, price:150.00})
+  }
+
+  onRemoveCourse(course: { title: string; students: number; rating: number; price: number; }){
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index,1);
+  }
+
+  // used in gnSwitchCase
   viewMode = 'courses';
 }
