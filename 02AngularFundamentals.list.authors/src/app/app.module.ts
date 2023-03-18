0749bd6e89df3ceb6e47bd6b7ff7ba897523b67d
Daniela_Rigoli <Daniela_Rigoli@dell.com>
2023-03-18 14:24:59 -0300

    building a bootstrap form

--- 02AngularFundamentals.list.authors/src/app/app.module.ts
+++ 02AngularFundamentals.list.authors/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCastingComponent } from './title-casting/title-casting.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ZippyComponent } from './zippy/zippy.component';
     FavoriteComponent,
     TitleCastingComponent,
     InputFormatDirective,
-    ZippyComponent
+    ZippyComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
