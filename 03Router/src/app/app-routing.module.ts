a4bdeb2f79c4d63826ee5ee41efd615617b90d48
Daniela_Rigoli <Daniela_Rigoli@dell.com>
2023-04-21 12:44:58 -0300

    Demo/Router Lazy Loaded Modules

--- 03Router/src/app/app-routing.module.ts
+++ 03Router/src/app/app-routing.module.ts
@@ -1,14 +1,27 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
-
+  {
+      path: "courses",
+      loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  },
+  {
+      path: "login",
+      component: LoginComponent
+  },
+  {
+      path: "about",
+      component: AboutComponent
+  }
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forChield(routes)
   ],
   exports: [RouterModule],
   providers: [
