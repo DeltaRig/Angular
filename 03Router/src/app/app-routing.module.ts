8266e9ae03d94acacaab8c130b47353bde5ac7a9
DeltaRig <daniprigoli@gmail.com>
2023-04-22 07:23:59 -0300

    Config Router

--- 03Router/src/app/app-routing.module.ts
+++ 03Router/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -16,12 +17,16 @@ const routes: Routes = [
   {
       path: "about",
       component: AboutComponent
+  }, 
+  {
+    path: "**",
+    component: PageNotFoundComponent
   }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forChield(routes)
+    RouterModule.forChild(routes)
   ],
   exports: [RouterModule],
   providers: [
