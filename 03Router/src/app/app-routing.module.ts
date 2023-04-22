583131193a2bb31d0f5901fe7c492acd5edfd3ff
DeltaRig <daniprigoli@gmail.com>
2023-04-22 14:52:52 -0300

    fix router project

--- 03Router/src/app/app-routing.module.ts
+++ 03Router/src/app/app-routing.module.ts
@@ -1,36 +1,62 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { AboutComponent } from './about/about.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {Routes, RouterModule, PreloadAllModules, UrlSerializer, NoPreloading} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {AboutComponent} from './about/about.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {CanLoadAuthGuard} from './services/can-load-auth.guard';
+import {CustomPreloadingStrategy} from './services/custom-preloading.strategy';
+import {ChatComponent} from './chat/chat.component';
 
 
 const routes: Routes = [
-  {
+    {
+      path: "",
+      redirectTo: "/courses",
+        pathMatch: "full"
+    },
+    {
       path: "courses",
-      loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
-  },
-  {
-      path: "login",
-      component: LoginComponent
-  },
-  {
-      path: "about",
-      component: AboutComponent
-  }, 
-  {
-    path: "**",
-    component: PageNotFoundComponent
-  }
+      loadChildren: () => import('./courses/courses.module')
+                            .then(m => m.CoursesModule),
+        // canLoad: [CanLoadAuthGuard]
+       data: {
+          preload: false
+       }
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    },
+    {
+        path: 'helpdesk-chat',
+        component: ChatComponent,
+        outlet: 'chat'
+    },
+    {
+        path: "**",
+        component: PageNotFoundComponent
+    }
+
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forChild(routes)
+      RouterModule.forRoot(
+          routes, {
+    preloadingStrategy: CustomPreloadingStrategy,
+    scrollPositionRestoration: 'enabled',
+    paramsInheritanceStrategy: 'always',
+    malformedUriErrorHandler: (error: URIError, urlSerializer: UrlSerializer, url: string) => urlSerializer.parse("/page-not-found")
+})
   ],
   exports: [RouterModule],
   providers: [
-
+      CanLoadAuthGuard,
+      CustomPreloadingStrategy
   ]
 })
 export class AppRoutingModule {
