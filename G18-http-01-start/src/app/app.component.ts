f3f9a8ed6e5e5b0a0c425fbe047f8f35ea1b3d77
DeltaRig <daniprigoli@gmail.com>
2023-07-28 15:09:59 -0300

    rxjs operators to transform response data

--- G18-http-01-start/src/app/app.component.ts
+++ G18-http-01-start/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -26,9 +30,28 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://ng-complete-guide-ce9c9-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData => {
+      // convert javascript object in array
+      const postsArray = [];
+      for (const key in responseData) {
+        if(responseData.hasOwnProperty(key)) { // check if has own property to don't try access a property or prototype
+          postsArray.push({...responseData[key], id: key})
+        }
+      }
+      return postsArray;
+    }
+    ))
+    .subscribe(posts=> {
+
+    });
   }
 }
